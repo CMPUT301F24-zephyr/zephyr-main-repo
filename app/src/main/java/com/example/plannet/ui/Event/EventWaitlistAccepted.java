ad04a4cd54c796fb82130f368e0954520d4c6962
Tejas Narayan <tejasnarayan123@gmail.com>
2024-10-27 01:35:39 -0600

    Added EntrantWaitlistAccepted, EntrantWaitlistRejected, EventWaitlistAccepted, EventWaitlistRejected, EventWaitlistChosen classes

--- app/src/main/java/com/example/plannet/ui/Event/EventWaitlistAccepted.java
+++ app/src/main/java/com/example/plannet/ui/Event/EventWaitlistAccepted.java
@@ -1,17 +1,17 @@
 package com.example.plannet.ui.Event;
 
-import com.example.plannet.ui.Entrant.Entrant;
+import com.example.plannet.ui.Entrant.EntrantProfile;
 
 import java.util.ArrayList;
 
 public class EventWaitlistAccepted {
-    private ArrayList<Entrant> acceptedEntrants;
+    private ArrayList<EntrantProfile> acceptedEntrants;
 
-    public EventWaitlistAccepted(ArrayList<Entrant> acceptedEntrants) {
+    public EventWaitlistAccepted(ArrayList<EntrantProfile> acceptedEntrants) {
         this.acceptedEntrants = new ArrayList<>();
     }
 
-    public void addEntrant(Entrant entrant) {
+    public void addEntrant(EntrantProfile entrant) {
         if (entrant != null) {
             this.acceptedEntrants.add(entrant);
         }
--- app/src/main/java/com/example/plannet/ui/Event/EventWaitlistChosen.java
+++ app/src/main/java/com/example/plannet/ui/Event/EventWaitlistChosen.java
@@ -1,23 +1,23 @@
 package com.example.plannet.ui.Event;
 
-import com.example.plannet.ui.Entrant.Entrant;
+import com.example.plannet.ui.Entrant.EntrantProfile;
 
 import java.util.ArrayList;
 
 public class EventWaitlistChosen {
-    private ArrayList<Entrant> chosenEntrants;
+    private ArrayList<EntrantProfile> chosenEntrants;
 
     public EventWaitlistChosen() {
         this.chosenEntrants = new ArrayList<>();
     }
 
-    public void addChosenEntrant(Entrant entrant) {
+    public void addChosenEntrant(EntrantProfile entrant) {
         if (entrant != null) {
             this.chosenEntrants.add(entrant);
         }
     }
 
-    public void removeChosenEntrant(Entrant entrant) {
+    public void removeChosenEntrant(EntrantProfile entrant) {
         if (entrant != null) {
             this.chosenEntrants.remove(entrant);
         }
