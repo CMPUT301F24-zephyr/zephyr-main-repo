3cb6cbb790a0d11680311c045b28053412c622b5
Moe <mchamas@ualberta.ca>
2024-10-28 19:38:27 -0600

    added UUID functionality & logic if a first_time user or a concurrent user

--- app/src/main/java/com/example/plannet/MainActivity.java
+++ app/src/main/java/com/example/plannet/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.plannet;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,7 +15,8 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.plannet.databinding.ActivityMainBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.ListenerRegistration;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,14 +35,34 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_events)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_events, R.id.navigation_first_time_user)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
+        // hide the nav bar when on welcome screen
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.navigation_first_time_user) {
+                navView.setVisibility(View.GONE);
+            } else {
+                navView.setVisibility(View.VISIBLE);
+            }
+        });
+
         // Initialize db
         db = FirebaseFirestore.getInstance();
+        SharedPreferences sharedPreferences = getSharedPreferences("app_preferences", MODE_PRIVATE);
+        //Log.d("MainActivity", "CHECKPOINT");
+        //sharedPreferences.edit().remove("unique_id").apply(); // this is just for testing..
+        String uniqueID = sharedPreferences.getString("unique_id", null);
+
+        if (uniqueID == null) {
+            // run the welcome screen and when user presses "Get Started" button, generate UUID and place it in the db
+            // then re-direct to home fragment
+            navController.navigate(R.id.navigation_first_time_user);
+            //navView.setVisibility(View.GONE);
+        }
 
     }
 
