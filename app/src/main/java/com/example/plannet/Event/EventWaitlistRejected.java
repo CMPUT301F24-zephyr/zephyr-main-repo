d7833b104f175482785ee0d3bfdd508e636e0313
jfjeld15 <jfjeld@ualberta.ca>
2024-11-07 23:42:56 -0700

    - Updated OrganizerEventListArrayAdapter to match what Moe and I discussed in the lab. - added docstrings for: OrganizerEventListArrayAdapter, Event, EventList, EventWaitlistAccepted, EventWaitlistChosen, EventWaitlistPending, EventWaitlistRejected, Facility, OrganizerCreatedEvents, OrganizerProfile, FirstTimeUserFragment, EventsFragment, HomeFragment

--- app/src/main/java/com/example/plannet/Event/EventWaitlistRejected.java
+++ app/src/main/java/com/example/plannet/Event/EventWaitlistRejected.java
@@ -4,13 +4,25 @@ import com.example.plannet.Entrant.EntrantProfile;
 
 import java.util.ArrayList;
 
+/**
+ * Stores a list of all rejected entrants (chosen and declined) for an event's waitlist.
+ */
 public class EventWaitlistRejected {
     private ArrayList<EntrantProfile> rejectedEntrants;
 
+    /**
+     * Constructor. Initializes an empty ArrayList.
+     */
     public EventWaitlistRejected() {
         this.rejectedEntrants = new ArrayList<>();
     }
 
+    /**
+     * Add an entrant to the rejected list when they decline an invitation.
+     *
+     * @param entrant
+     *      The entrant to be added to the list.
+     */
     public void addEntrant(EntrantProfile entrant) {
         if (entrant != null) {
             this.rejectedEntrants.add(entrant);
