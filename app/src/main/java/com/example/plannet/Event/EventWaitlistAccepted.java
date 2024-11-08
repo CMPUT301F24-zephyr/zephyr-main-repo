36a9d6138b5c968ba6655a851f43b75445ce1acc
Tejas Narayan <tejasnarayan123@gmail.com>
2024-11-07 17:10:24 -0700

    Made fragments for entrant homescreen and entrant event list

--- app/src/main/java/com/example/plannet/Event/EventWaitlistAccepted.java
+++ app/src/main/java/com/example/plannet/Event/EventWaitlistAccepted.java
@@ -7,14 +7,36 @@ import java.util.ArrayList;
 
 public class EventWaitlistAccepted {
     private ArrayList<EntrantProfile> acceptedEntrants;
+    private String eventID;
+    private String type;
 
-    public EventWaitlistAccepted(ArrayList<EntrantProfile> acceptedEntrants) {
+    public EventWaitlistAccepted(String eventID) {
         this.acceptedEntrants = new ArrayList<>();
+        this.eventID = eventID;
+        this.type = "accepted";
     }
 
-    public void addEntrant(EntrantProfile entrant) {
-        if (entrant != null) {
-            this.acceptedEntrants.add(entrant);
-        }
+    public String getEventID() {
+        return eventID;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean addEntrant(EntrantProfile entrant) {
+        if (entrant != null && !this.acceptedEntrants.contains(entrant)) {
+            this.acceptedEntrants.add(entrant);
+            return true;
+        }
+        return false;
+    }
+    public boolean removeEntrant(EntrantProfile entrant) {
+        if (entrant != null && this.acceptedEntrants.contains(entrant)) {
+            this.acceptedEntrants.remove(entrant);
+            return true;
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/example/plannet/Event/EventWaitlistRejected.java
+++ app/src/main/java/com/example/plannet/Event/EventWaitlistRejected.java
@@ -6,14 +6,36 @@ import java.util.ArrayList;
 
 public class EventWaitlistRejected {
     private ArrayList<EntrantProfile> rejectedEntrants;
+    private String eventID;
+    private String type;
 
-    public EventWaitlistRejected() {
+    public EventWaitlistRejected(String eventID) {
         this.rejectedEntrants = new ArrayList<>();
+        this.eventID = eventID;
+        this.type = "rejected";
     }
 
-    public void addEntrant(EntrantProfile entrant) {
-        if (entrant != null) {
+    public String getEventID() {
+        return eventID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean addEntrant(EntrantProfile entrant) {
+        if (entrant != null && !this.rejectedEntrants.contains(entrant)) {
             this.rejectedEntrants.add(entrant);
+            return true;
         }
+        return false;
+    }
+
+    public boolean removeEntrant(EntrantProfile entrant) {
+        if (entrant != null && this.rejectedEntrants.contains(entrant)) {
+            this.rejectedEntrants.remove(entrant);
+            return true;
+        }
+        return false;
     }
 }
