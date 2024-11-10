e384badae453ce9592e74c9542d35e7e0dd004d0
moe chamas <moechamas@MacBook-Pro.local>
2024-11-10 11:46:43 -0700

    instrumental tests fixed -- fragment classes need to have view hierarchy for tests to always pass

--- app/src/androidTest/java/com/example/plannet/ProjectInstrumentedTest.java
+++ app/src/androidTest/java/com/example/plannet/ProjectInstrumentedTest.java
@@ -55,6 +55,10 @@ public class ProjectInstrumentedTest {
     public void testOrganizerCreateNewEventFragmentUIComponents() {
         checkFirstTimeUserFragment();
         Espresso.onIdle();
+
+        onView(withId(R.id.button_switch2)).perform(click());
+        Espresso.onIdle();
+
         onView(withId(R.id.buttonNewEvent)).check(matches(isDisplayed()));
         Espresso.onIdle();
         // Check OrganizerCreateEventFragment UI elements
@@ -74,11 +78,11 @@ public class ProjectInstrumentedTest {
         Espresso.onIdle();
 
         // Click on organizer profile tab from nav bar
-        onView(withId(R.id.navigation_orgprofile)).perform(click());
+        onView(withId(R.id.navigation_entrantprofile)).perform(click());
         Espresso.onIdle();
 
-        // Verify that content specific to Orgpprofile is displayed
-        onView(withId(R.id.facility_prompt)).check(matches(isDisplayed()));
+        // Verify that content specific to entrantpprofile is displayed
+        onView(withId(R.id.first_name_edittext)).check(matches(isDisplayed()));
         Espresso.onIdle();
     }
 
@@ -90,6 +94,10 @@ public class ProjectInstrumentedTest {
         // Get past first time user
         checkFirstTimeUserFragment();
         Espresso.onIdle();
+        // navigate to org view
+        onView(withId(R.id.button_switch2)).perform(click());
+        Espresso.onIdle();
+
         // click create new event
         onView(withId(R.id.buttonNewEvent)).perform(click());
         Espresso.onIdle();
