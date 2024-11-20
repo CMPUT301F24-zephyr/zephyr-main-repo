7304ef0fb24defb4bb2e6ffa0cb22795eedd85c4
moe chamas <moechamas@MacBook-Pro.local>
2024-11-19 20:34:27 -0700

    fixed some bugs with functionality, notifications in progress -- don't delete branch

--- app/src/main/java/com/example/plannet/MainActivity.java
+++ app/src/main/java/com/example/plannet/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             // Nav tabs configuration depending on which role (later add admin as well)
-            if (destination.getId() == R.id.navigation_entranthome || destination.getId() == R.id.navigation_entrantprofile || destination.getId() == R.id.navigation_entrantnotifications) {
+            if (destination.getId() == R.id.navigation_entranthome || destination.getId() == R.id.navigation_entrantprofile || destination.getId() == R.id.navigation_entrantnotifications || destination.getId() == R.id.navigation_entrant_profile_display) {
                 if (check != true) {
                     // Customize tabs for EntrantHomeFragment
                     showEntrantTabs();
