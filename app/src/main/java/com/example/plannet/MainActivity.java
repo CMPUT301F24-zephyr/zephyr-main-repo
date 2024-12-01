bfeb7366c58644362daf5786e5409b144790ee7a
moe chamas <mchamas5@hotmail.com>
2024-12-01 15:06:32 -0700

    main activity redundant code remove

--- app/src/main/java/com/example/plannet/MainActivity.java
+++ app/src/main/java/com/example/plannet/MainActivity.java
@@ -49,15 +49,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         BottomNavigationView navView = binding.navView;
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_orghome, R.id.navigation_orgprofile, R.id.navigation_sendnotification, R.id.navigation_first_time_user, R.id.organizerHashedQrListFragment,
-                R.id.navigation_organizer_create_event, R.id.navigation_entranthome, R.id.navigation_qr_code_scan,
-                R.id.navigation_event_details, R.id.navigation_entrantprofile, R.id.navigation_entrantnotifications, R.id.navigation_notificationmanager,
-                R.id.navigation_entrantprofile, R.id.navigation_event_details, R.id.navigation_entrant_profile_display, R.id.navigation_event_list,
-                R.id.organizerViewEventFragment, R.id.organizerViewEntrantsFragment, R.id.organizerViewEntrantInfoFragment, R.id.navigation_adminhome, R.id.navigation_entrantmap)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
         // Hide the navigation bar on the welcome screen
