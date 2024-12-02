73a570d61a04cc836e42fe2b0e45796c4ea75551
Tejas Narayan <tejasnarayan123@gmail.com>
2024-12-02 13:17:09 -0700

    Updated entrant testing.

--- app/src/androidTest/java/com/example/plannet/EntrantTest.java
+++ app/src/androidTest/java/com/example/plannet/EntrantTest.java
@@ -6,13 +6,23 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.View;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.util.HumanReadables;
+import androidx.test.espresso.util.TreeIterables;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -23,6 +33,8 @@ import androidx.test.uiautomator.UiDevice;
 import com.example.plannet.MainActivity;
 import com.example.plannet.R;
 
+import org.hamcrest.Matcher;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,14 +45,22 @@ import org.junit.runner.RunWith;
 public class EntrantTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
-
     @Before
-    public void preSetup() {
-        Espresso.onIdle();
-        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-        if (device.hasObject(By.text("Allow"))) {
-            device.findObject(By.text("Allow")).click();
-        }
+    public void setUpPreferences() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        SharedPreferences sharedPreferences = context.getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isFirstTime", false); //set first-time flag to false
+        editor.apply();
+    }
+
+    @After
+    public void clearPreferences() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        SharedPreferences sharedPreferences = context.getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear(); //clear all preferences after each test
+        editor.apply();
     }
 
     @Test
@@ -50,29 +70,24 @@ public class EntrantTest {
         onView(withId(R.id.profile_picture)).check(matches(isDisplayed()));
     }
 
-    @Test
-    public void testSuccessfulQRScanNavigatesToEventDetail() {
-        onView(withId(R.id.scan_qr_button)).perform(click());
-        onView(withId(R.id.barcode_scanner)).perform(typeText("SampleEventQRData"));
-        onView(withId(R.id.entrants_button)).check(matches(isDisplayed()));
-    }
 
-    @Test
-    public void testNavigateToProfile() {
-        onView(withId(R.id.profile_picture)).perform(click());
-        onView(withId(R.id.profile_text)).check(matches(isDisplayed()));
-    }
+
+
 
     @Test
     public void testNavigateToEventList() {
         onView(withId(R.id.view_events_button)).perform(click());
-        onView(withId(R.id.event_item_container)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.event_item_container))
+                .check(matches(isDisplayed()));
     }
 
     @Test
     public void testBackNavigationFromEventList() {
         onView(withId(R.id.view_events_button)).perform(click());
+
         onView(isRoot()).perform(ViewActions.pressBack());
+
         onView(withId(R.id.scan_qr_button)).check(matches(isDisplayed()));
         onView(withId(R.id.view_events_button)).check(matches(isDisplayed()));
     }
@@ -80,29 +95,34 @@ public class EntrantTest {
     @Test
     public void testUIElementsOnScanPage() {
         onView(withId(R.id.scan_qr_button)).perform(click());
-        onView(withId(R.id.barcode_scanner)).check(matches(isDisplayed()));
-    }
 
-    @Test
-    public void testViewEventsListViewItemClick() {
-        onView(withId(R.id.view_events_button)).perform(click());
-        onView(withId(R.id.event_item_container)).perform(click());
-        onView(withId(R.id.entrant_View_Event_Fragment)).check(matches(isDisplayed()));
+        onView(withId(R.id.barcode_scanner)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testBottomNavigationBarNavigationEntrantProfile() {
         onView(withId(R.id.navigation_entranthome)).perform(click());
-        onView(withId(R.id.container)).check(matches(isDisplayed()));
+        onView(withId(R.id.container))
+                .check(matches(isDisplayed()));
+
+
         onView(withId(R.id.navigation_entrant_profile_display)).perform(click());
-        onView(withId(R.id.profile_text)).check(matches(isDisplayed()));
+        onView(withId(R.id.profile_text))
+                .check(matches(isDisplayed()));
+
     }
 
     @Test
     public void testBottomNavigationBarNavigationEntrantNotif() {
         onView(withId(R.id.navigation_entranthome)).perform(click());
-        onView(withId(R.id.container)).check(matches(isDisplayed()));
+        onView(withId(R.id.container))
+                .check(matches(isDisplayed()));
+
+
         onView(withId(R.id.navigation_entrantnotifications)).perform(click());
-        onView(withId(R.id.invite_list_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.invite_list_view))
+                .check(matches(isDisplayed()));
     }
+
+
 }
